af7d63c4f099558f7a1c52caf0fc5d2653ffb359
王一航 <997918108@qq.com>
2016-09-27 11:30:29 +0800

    凯撒密码
    只转换字母(包括大写小写) , 其余字符原样打印

---
+++ Cesar.py
@@ -0,0 +1,26 @@
+# coding:utf8
+import sys
+
+def cesar(ciphertext):
+	'''
+	凯撒密码 :
+		只转换字母(包括大写小写)
+	'''
+	result = ""
+	for i in range(26):
+		for ch in ciphertext:
+			if (ord(ch) >= ord('A')) and (ord(ch) <= ord('Z')):
+				newch = chr(((ord(ch) - ord('A') + i) % 26) + ord('Z'))
+			elif (ord(ch) >= ord('a')) and (ord(ch) <= ord('z')):
+				newch = chr(((ord(ch) - ord('a') + i) % 26) + ord('a'))
+			else:
+				newch = ch
+			result += newch
+			# sys.stdout.write(newch)
+		result += "\n"
+		# sys.stdout.write("\n");
+	return result
+
+ciphertext = "abcdefghijklmnopqrstuvwxyz.1234567890/*-+"
+
+print cesar(ciphertext)
