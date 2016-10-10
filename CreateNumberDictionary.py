244d7f595c468ed5443980a0bcc259a6e865557b
王一航 <997918108@qq.com>
2016-10-10 11:27:57 +0800

    生成数字类型密码字典
    修复单词拼写错误

--- CreateNumberDictionary.py
+++ CreateNumberDictionary.py
@@ -1,5 +1,5 @@
 # coding:utf8
-def creatDictionary(length, start, end):
+def createDictionary(length, start, end):
 	'''
 		Length:
 			生成的单个密码的长度
@@ -15,4 +15,4 @@ def creatDictionary(length, start, end):
 				temp = "0" + temp;
 		print temp;
 		
-creatDictionary(4,0,10000); # 注意数组从0开始索引
+createDictionary(4,0,10000); # 注意数组从0开始索引
