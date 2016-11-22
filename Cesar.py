e77d567a9f97c23d63ef689bf8e266eee4476659
王一航 <wangyihanger@gmail.com>
2016-11-22 10:24:24 +0800

    修正凯撒加密
    结构更加合理 , 修改算法

--- Cesar.py
+++ Cesar.py
@@ -1,26 +1,35 @@
-# coding:utf8
-import sys
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
 
-def cesar(ciphertext):
+upperDict=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lowerDict=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def cesarWithLetter(ciphertext,offset):
 	'''
 	凯撒密码 :
 		只转换字母(包括大写小写)
+	参数 : 
+		ciphertext : 明文
+		offset : 偏移量
 	'''
 	result = ""
-	for i in range(26):
-		for ch in ciphertext:
-			if (ord(ch) >= ord('A')) and (ord(ch) <= ord('Z')):
-				newch = chr(((ord(ch) - ord('A') + i) % 26) + ord('A'))
-			elif (ord(ch) >= ord('a')) and (ord(ch) <= ord('z')):
-				newch = chr(((ord(ch) - ord('a') + i) % 26) + ord('a'))
-			else:
-				newch = ch
-			result += newch
-			# sys.stdout.write(newch)
-		result += "\n"
-		# sys.stdout.write("\n");
+	for ch in ciphertext:
+		if ch.isupper():
+			result=result+upperDict[((upperDict.index(ch)+offset)%26)]
+		elif ch.islower():
+			result=result+lowerDict[((lowerDict.index(ch)+offset)%26)]
+		elif ch.isdigit():
+			result=result+ch
+		else:
+			result=result+ch
 	return result
 
-ciphertext = "abcdefghijklmnopqrstuvwxyz.1234567890/*-+"
+def printAllResult(ciphertext):
+	'''
+	打印所有偏移结果
+	'''
+	for i in range(len(upperDict)):
+		print cesarWithLetter(ciphertext,i)
 
-print cesar(ciphertext)
+ciphertext=input("Please input the words : ")
+printAllResult(ciphertext)
