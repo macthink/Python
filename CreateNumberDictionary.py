22e03a50d0654176796c55641b40eeed67889496
王一航 <wangyihanger@gmail.com>
2016-11-22 10:46:36 +0800

    优化代码结构

--- CreateNumberDictionary.py
+++ CreateNumberDictionary.py
@@ -1,12 +1,16 @@
-# coding:utf8
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+
 def createDictionary(length, start, end):
 	'''
+	功能 : 生成指定长度,指定起始位置,结束位置的数字类型密码字典
+	参数 : 
 		Length:
 			生成的单个密码的长度
 		Start:
-			起始数字
+			起始位置
 		End:
-			结束数字
+			结束位置
 	'''
 	for i in range(start, end): # 如果需要很大长度的密码字典 , 为了节省内存 , 需要讲range换成xrange
 		temp = str(i);
