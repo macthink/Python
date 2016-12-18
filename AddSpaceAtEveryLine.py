098de71bb841eec66492700a84bb22db4e0b3aef
王一航 <wangyihanger@gmail.com>
2016-12-19 02:34:02 +0800

    Update AddSpaceAtEveryLine.py

--- AddSpaceAtEveryLine.py
+++ AddSpaceAtEveryLine.py
@@ -1,25 +1,28 @@
-#coding:utf8
-# 功能介绍 : 将文本文件中每一行的结尾都加上两个空格
-# 应用场景 : 将简书中的MarkDown向Github中移植的时候总是会出现不换行的问题 , 此脚本主要用于解决该问题
-# 作者 : 王一航
-# 时间 : 2016/09/17
-fileName = "README.md" # 文件名
+#!/usr/bin/env python
+# encoding:utf8
+
+import sys
+
+if len(sys.argv) != 2:
+	print "Usage : \r\n\tpython " + sys.argv[0] + " README.md"
+	exit(1)
+fileName = sys.argv[1] # 文件名
 file = open(fileName, "r")
 lines = file.readlines() # 一次性将文件全部读进内存
 number = len(lines); # 获取行数
 for i in range(0, number):
-	if lines[i].endswith("  \n"):
+	if lines[i].endswith("  \r\n"):
 		continue
-	elif lines[i].endswith(" \n"):
-		lines[i] = lines[i][:-2] + "  \n"
+	elif lines[i].endswith(" \r\n"):
+		lines[i] = lines[i][:-2] + "  \r\n"
 	elif lines[i].endswith("\n"):
-		lines[i] = lines[i][:-1] + "  \n"
+		lines[i] = lines[i][:-1] + "  \r\n"
 	else:
-		lines[i] += "  \n" # 处理文件结尾
+		lines[i] += "  \r\n" # 处理文件结尾
 file.close()
 
 # 将文件内容的列表写入文件
-file = open(fileName, "w+")
+file = open(fileName, "w")
 for i in range(0, number):
 	file.write(lines[i])
 file.close()
